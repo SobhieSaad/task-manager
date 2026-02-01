273e09227e3be46fb198b714fc28a501d1df4dc2
SobhieSaad <sobhie.saad1@gmail.com>
2026-02-01 21:37:25 +0100

    prepare the auth routes

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,3 +1,15 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    //index("routes/root/HomePage.tsx")
+
+    layout(
+        'routes/auth/auth-layout.tsx', [
+            index('routes/auth/sign-in.tsx'),
+            index('routes/auth/sign-up.tsx'),
+            index('routes/auth/foreget-password.tsx'),
+            index('routes/auth/reset-password.tsx'),
+            index('routes/auth/verify-email.tsx'),
+        ]
+    )
+] satisfies RouteConfig;
